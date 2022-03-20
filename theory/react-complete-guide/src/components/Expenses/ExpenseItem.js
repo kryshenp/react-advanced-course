3728693ab872b4f5b69b947ee1975fbd2ee75e34
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-03-20 13:42:12 +0100

    wip

--- theory/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ theory/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,17 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log("CLICKED!!!");
+    title = "UPDATED";
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
