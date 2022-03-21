ba34a75341dc9353bd1f4fe4612902aeb60d4b6d
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-03-21 23:24:24 +0100

    4 61 stateless vs stateful components

--- theory/react-complete-guide/src/components/Expenses/Expenses.js
+++ theory/react-complete-guide/src/components/Expenses/Expenses.js
@@ -16,7 +16,8 @@ const Expenses = (props) => {
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={selectedYear} onChangeYearFilter={yearChangeDataHandler} />
+        {/* this is a controlled component */}
+        <ExpensesFilter selected={selectedYear} onChangeYearFilter={yearChangeDataHandler} /> {/* this is a controlled component */}
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
