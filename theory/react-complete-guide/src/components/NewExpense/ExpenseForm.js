e0c7e13a11cdd7bd015460d15ce607b1ab279fff
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-03-29 11:22:11 +0200

    add assignment solution

--- theory/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ theory/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -6,59 +6,18 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
-  // });
+  const [showForm, setShowForm] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: event.target.value,
-    // });
-
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredTitle: event.target.value,
-    //   };
-    // });
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
-
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredAmount: event.target.value,
-    //   };
-    // });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
-
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredDate: event.target.value,
-    //   };
-    // });
   };
 
   const submitHandler = (event) => {
@@ -77,25 +36,34 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
+  const showFormHandler = () => setShowForm(true);
+  const hideFormHandler = () => setShowForm(false);
+
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
+      {!showForm && <button onClick={showFormHandler}>Add Expense</button>}
+      {showForm ? (
+        <div>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate} />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button onClick={hideFormHandler}>Cancel</button>
+            <button type="submit">Add Expense</button>
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate} />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
+      ) : null}
     </form>
   );
 };
