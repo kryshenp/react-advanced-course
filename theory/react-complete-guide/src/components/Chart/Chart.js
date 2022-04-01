07fd7d007f10c0bd2e319f35a7bce1d9faa0e612
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-04-01 22:07:02 +0200

    5 70 add dynamic styles to chart bar component

--- theory/react-complete-guide/src/components/Chart/Chart.js
+++ theory/react-complete-guide/src/components/Chart/Chart.js
@@ -5,10 +5,13 @@ import "./Chart.css";
 
 // functional component
 const Chart = (props) => {
+  const dataPointvalues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointvalues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => {
-        <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label} />;
+        return <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />;
       })}
     </div>
   );
