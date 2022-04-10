231761895a50f6de632b38d8d13175bb7309e1f6
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-04-10 22:16:26 +0200

    8 89 Add main logic

---
+++ complete-practice/src/components/NewUser/UserForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import Button from "../UI/Button";
+
+const UserForm = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const userNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const userAgeChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const userData = {
+      name: enteredName,
+      age: enteredAge,
+    };
+
+    props.onSaveUserData(userData);
+
+    setEnteredName("");
+    setEnteredAge("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-user__controls">
+        <div className="new-user__control">
+          <label>Username</label>
+          <input type="text" onChange={userNameChangeHandler} value={enteredName} />
+        </div>
+        <div className="new-user__control">
+          <label>Age (years)</label>
+          <input type="number" step="1" onChange={userAgeChangeHandler} value={enteredAge} />
+        </div>
+      </div>
+      <div className="new-user__actions">
+        <Button label="Add User" type="submit" />
+      </div>
+    </form>
+  );
+};
+
+export default UserForm;
