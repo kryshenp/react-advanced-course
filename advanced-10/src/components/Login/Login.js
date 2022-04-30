692fd87ecaa2b40f86f7751743fb877f7a1ede8d
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-04-30 20:34:00 +0200

    10 118 useReducer in combo with useEffect

--- advanced-10/src/components/Login/Login.js
+++ advanced-10/src/components/Login/Login.js
@@ -27,45 +27,47 @@ const passwordReducer = (state, action) => {
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: "", isValid: null });
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: "", isValid: null });
 
-  // useEffect(() => {
-  //   console.log("EFFECT RUNNING");
+  useEffect(() => {
+    console.log("EFFECT RUNNING");
 
-  //   return () => {
-  //     console.log("EFFECT CLEANUP");
-  //   };
-  // }, [enteredPassword]);
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   const indentifier = setTimeout(() => {
-  //     console.log("Checking form validity!");
-  //     setFormIsValid(enteredEmail.includes("@") && enteredPassword.trim().length > 6);
-  //   }, 500);
+  const { isValid: emailIsValid } = emailState; // this is an alias assignment, NO value assignment
+  const { isValid: passwordIsValid } = passwordState;
 
-  //   return () => {
-  //     clearTimeout(indentifier);
-  //     console.log("CLEANUP"); // this doesn't run before the very first side effect execution
-  //   };
-  // }, [enteredEmail, enteredPassword]);
+  useEffect(() => {
+    const indentifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      clearTimeout(indentifier);
+      console.log("CLEANUP"); // this doesn't run before the very first side effect execution
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
+    // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
-    // setFormIsValid(event.target.value.trim().length > 6 && enteredEmail.includes("@"));
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
