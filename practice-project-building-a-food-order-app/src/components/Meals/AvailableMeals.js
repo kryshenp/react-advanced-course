0c010a69d9eaae9236d250b22d7500b188e48f97
Pavlo Kryshenyk <pkryshenyk-cf@pkryshenyk-2PC32-cz-ltmp-x.local>
2022-06-26 16:37:53 +0200

    10

--- practice-project-building-a-food-order-app/src/components/Meals/AvailableMeals.js
+++ practice-project-building-a-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -32,9 +32,8 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
-    <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />;
-  });
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+
   return (
     <section className={classes.meals}>
       <Card>
