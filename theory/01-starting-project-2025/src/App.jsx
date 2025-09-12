d641a4b431e46cdc314b51142c14e7c1d762c686
Pavlo Kryshenyk <kryshenykpavlo@gmail.com>
2025-09-12 20:38:46 +0200

    3-56. Rendering Content Conditionally-pt2

--- theory/01-starting-project-2025/src/App.jsx
+++ theory/01-starting-project-2025/src/App.jsx
@@ -55,9 +55,8 @@ function App() {
                             State
                         </TabButton>
                     </menu>
-                    {!selectedTopic ? (
-                        <p>Please select a topic.</p>
-                    ) : (
+                    {!selectedTopic && <p>Please select a topic.</p>}
+                    {selectedTopic && (
                         <div id="tab-content">
                             <h3>{EXAMPLES[selectedTopic].title}</h3>
                             <p>{EXAMPLES[selectedTopic].description}</p>
