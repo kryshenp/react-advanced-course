1c572747255040c9a31f0cdb9d0beecfbb26d791
Pavlo Kryshenyk <kryshenykpavlo@gmail.com>
2025-10-05 21:18:55 +0200

    4-75. Concept Repetition: Working with State-MY-TASK-SOLUTION

--- theory/07-tic-tac-toe-starting-project-2025/src/components/Player.jsx
+++ theory/07-tic-tac-toe-starting-project-2025/src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        console.log("EDITING...");
+        setIsEditing(true);
+    }
+
+    let editElementContent;
+    editElementContent = !isEditing ? (
+        <span className="player-name">{name}</span>
+    ) : (
+        <input type="text" />
+    );
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editElementContent}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => handleEdit()}>Edit</button>
         </li>
     );
 }
